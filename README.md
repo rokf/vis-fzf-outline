9222938c9d2e65844e805329c81aba08ec725907
Rok Fajfar <rok.fajfar@3fs.si>
2020-10-17 17:01:26 +0200

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+# vis-fzf-outline
+
+This repository contains an outline navigation plugin for the [Vis](https://github.com/martanne/vis) editor.
+
+## Installation
+
+Clone the repository into your Vis configuration folder:
+
+```sh
+cd ~/.config/vis
+git clone https://github.com/rokf/vis-fzf-outline
+```
+
+Import the plugin in your `visrc.lua` file:
+
+```lua
+require('vis-fzf-outline/init')
+```
+
+## Usage
+
+Requiring this plugin's `init.lua` file (see installation section above) should register an `outline` command. Executing the command should show a list of matching outline entries for the current syntax. Each syntax can have multiple outline matchers - the appropriate one is chosen through a matcher function, which expects a File structure. The specific matcher is chosen if it returns a truthy value.
+
+The `init.lua` file contains a few default outline matchers.
+
+### Registering your own matchers
+
+The plugin returns its matcher table and it can be modified. Example (`visrc.lua`):
+
+```lua
+local outline = require('vis-fzf-outline/init')
+
+outline['lua'][
+	function (file)
+		return string.match(file.name, '_test.lua$')
+	end
+] = {
+	'^%s*describe'
+}
+```
+
+## License
+
+This library is free software; you can redistribute it and/or modify it under the terms of the MIT license. See LICENSE for details.
---
+++ init.lua
@@ -0,0 +1,104 @@
+local m = {
+	['go'] = {
+		[function () return true end] = {
+			'type ',
+			'func '
+		}
+	},
+	['lua'] = {
+		[function () return true end] = {
+			'^function ',
+			'^local '
+		}
+	},
+	['makefile'] = {
+		[function () return true end] = {
+			'^[a-zA-Z0-9/]+:' -- targets
+		}
+	},
+	['yaml'] = {
+		[function (file) return string.match(file.name, 'docker%-compose') end] = {
+			'^  [a-zA-Z0-9/-]+:' -- two space prefix
+		},
+		[function () return true end] = {
+			'^  [A-Z]%a*:$', -- definitions, parameters
+			'^  /[^:]+:$' -- paths
+		}
+	},
+	['markdown'] = {
+		[function (file) return string.match(file.name, 'CHANGELOG') end] = {
+			'^##%s+' -- H2
+		},
+		[function () return true end] = {
+			'^#+%s+' -- any header
+		}
+	},
+	['ini'] = {
+		[function () return true end] = {
+			'^%[[^]]+%]$'
+		}
+	}
+}
+
+local info = function (cmd, fmt, ...)
+	vis:info(
+		string.format(
+			'vis-fzf-outline: [%s] %s',
+			cmd,
+			string.format(fmt, ...)
+		)
+	)
+end
+
+vis:command_register('outline', function (argv,force,win,selection,range)
+	local matchers = m[win.syntax]
+	if matchers ~= nil and type(matchers) == 'table' then
+		for k, v in pairs(matchers) do
+			if k(win.file) then
+				local matching_lines = {}
+
+				for i, line in ipairs(win.file.lines) do
+					for _, pattern in ipairs(v) do
+						if string.match(line, pattern) then
+							table.insert(matching_lines, string.format(
+								'%-5d %s', i, line
+							))
+							break
+						end
+					end
+				end
+
+				if #matching_lines == 0 then
+					info('outline', 'no matching lines')
+					return true
+				end
+
+				local fzf = io.popen(string.format(
+					"echo '%s' | fzf --no-sort --tac",
+					table.concat(matching_lines, '\n')
+				))
+				local out = fzf:read()
+				local success, msg, status = fzf:close()
+
+				if status == 0 then
+					local line_number = string.match(out, '^(%d+)%s+')
+					if line_number ~= nil then
+						selection:to(line_number, 1)
+					end
+				elseif status ~= 130 then -- not exit
+					info('outline', 'error running fzf %s', msg)
+				end
+
+				vis:redraw()
+
+				return true -- end here, skip remaining matchers
+			end
+		end
+	else
+		info('outline', 'no matchers registered for syntax "%s"', win.syntax)
+	end
+
+	return true
+end)
+
+return m
